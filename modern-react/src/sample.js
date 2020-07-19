793cce8c2a3413120f5520f2d0dd7e788faed728
marvelopers <marvelopers@gmail.com>
2020-07-19 22:42:51 +0900

    add snippet-generator

---
+++ modern-react/src/sample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function sample() {
+    return (
+        <div>
+            MyComponent
+        </div>
+    );
+}
+
+export default sample;
+//https://snippet-generator.app/
