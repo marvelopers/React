e4be98bb91b9a22bb30713a54b855c9ed2f90517
marvelopers <marvelopers@gmail.com>
2021-06-29 08:45:04 +0900

    컬러 파일 설정

---
+++ creating_profile/src/common/styles/colors.ts
@@ -0,0 +1,11 @@
+export const BLACK = 'black';
+export const WHITE = 'white';
+export const LIGHT_PINK = '#ffeae8';
+export const LIGHT_GREY = '#e8e7e6';
+export const GREY = '#626262';
+export const GREEN = '#385461';
+export const PINK = 'pink';
+export const SHADOW = 'rgba(217, 217, 217, 1)';
+export const COLORFUL =
+  'linear-gradient(166deg, rgba(237, 193, 211, 1) 0%,rgba(212, 149, 161, 1) 60%, rgba(162, 63, 63, 1) 100%, rgba(127, 188, 242, 1) 100%)';
+export const WHEAT = 'wheat';
